a0e20bd3d1399937d55e76e216f6316e544c29c3
Ruth Dantas <ruthdevgp@gmail.com>
2026-03-10 17:16:01 -0300

    refactor: centralizando test setup usando beforeEach

--- cypress/e2e/desafio.cy.js
+++ cypress/e2e/desafio.cy.js
@@ -4,19 +4,26 @@ import { user } from "../support/factories/userFactory"
 
 describe('Cadastro de usuário', () => {
 
-  it('Validar cadastro com sucesso', () => {
+  beforeEach('Validar cadastro', () => {
 
     cy.visit('/')
-      .get('.header-logo')    
+      .get('.header-logo')
     
-    cy.get('.fa-lock').click()
+    cy.get('.fa-lock')
+      .click()
+    
+    cy.url().should('include', '/register')
 
+    cy.get('#user')
+      .should('be.visible')  
+  })
+
+  it('Validar cadastro com sucesso', () => {
+      
     cy.get('#user').type(user.dadosValidos.name)
     cy.get('#email').type(user.dadosValidos.email)
     cy.get('#password').type(user.dadosValidos.senha)
 
-    console.log('nameeee:',user.dadosValidos.name)
-    console.log('emaillll:',user.dadosValidos.email)
 
     cy.get('#btnRegister').click()
 
@@ -32,34 +39,15 @@ describe('Cadastro de usuário', () => {
 
   })
   it('Validar todos os campos vazios', () => {
-    cy.visit('/')
-      .get('.header-logo')    
-    
-    cy.get('.fa-lock')
-      .click()
-      .url().should('include', '/register')
-      .get('#user')
-      .should('be.visible')
-      .get('#email')
-      .should('be.visible')
-      .get('#password')
-      .should('be.visible')
-      .get('#btnRegister')
-      .click()
-      .get('#errorMessageFirstName')
+     
+    cy.get('#btnRegister').click()
+
+    cy.get('#errorMessageFirstName')
       .should('be.visible')
       .should('have.text', 'O campo nome deve ser prenchido')
   })
   it('Validar campo com nome vazio', () => {
 
-    cy.visit('/')
-      .get('.header-logo')    
-    
-    cy.get('.fa-lock')
-      .click()
-      .url().should('include', '/register')
-      .get('#user')
-      .should('be.visible')
 
     cy.get('#email').type(user.dadosValidos.email)
     cy.get('#password').type(user.dadosValidos.senha)
@@ -73,14 +61,9 @@ describe('Cadastro de usuário', () => {
   })
   it('Validar campo com email vazio', () => {
 
-    cy.visit('/')
-      .get('.header-logo')    
-    
-    cy.get('.fa-lock').click()
-
     cy.get('#user')
-      .should('be.visible')
       .type(user.dadosValidos.name)
+      
     cy.get('#password').type(user.dadosValidos.senha)
 
     cy.get('#btnRegister').click()
@@ -92,12 +75,6 @@ describe('Cadastro de usuário', () => {
   })
   it('Validar com email inválido', () => {
 
-    cy.visit('/')
-      .get('.header-logo')    
-    
-    cy.get('.fa-lock').click()
-
-    
     cy.get('#user').type(user.dadosValidos.name)
     cy.get('#email').type(user.dadosInvalidos.emailInvalido)
     cy.get('#password').type(user.dadosValidos.senha)
@@ -111,13 +88,7 @@ describe('Cadastro de usuário', () => {
   })
   it('Validar campo com senha vazia', () => {
 
-    cy.visit('/')
-      .get('.header-logo')    
-    
-    cy.get('.fa-lock').click()
-
     cy.get('#user')
-      .should('be.visible')
       .type(user.dadosValidos.name)
 
     cy.get('#email')
@@ -131,21 +102,13 @@ describe('Cadastro de usuário', () => {
   })
   it('Validar campo com senha inválida', () => {
 
-    cy.visit('/')
-      .get('.header-logo')    
-    
-    cy.get('.fa-lock').click()
-
     cy.get('#user')
-      .should('be.visible')
       .type(user.dadosValidos.name) 
 
     cy.get('#email')
-      .should('be.visible')
       .type(user.dadosValidos.email)
 
     cy.get('#password')
-      .should('be.visible')
       .type(user.dadosInvalidos.senhaInvalida)
 
     cy.get('#btnRegister').click()
